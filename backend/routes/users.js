ade54598809f966410fb9645b4158b34cc1d2890
simonanderson16 <uhx8bu@virginia.edu>
2023-05-30 15:34:45 -0400

    test db in backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require("../firebase")
+
+
+const {collection, getDocs} = require("firebase/firestore")
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/db', function(req, res, next) {
+  getDocs(collection(db, "Users"))
+  .then((allDocs) => {allDocs.forEach((doc) => console.log(doc.id))})
+
+  res.send('respond with a resource');
+});
+
+
+
 
 module.exports = router;
