ca1cfb3b7fa6897ff1b0fc87fcc6e25fc0e797d9
ka1979 <118313969+ka1979@users.noreply.github.com>
2023-05-30 15:41:41 -0400

    routes

--- frontend/reactSpotify/src/App.jsx
+++ frontend/reactSpotify/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Route } from "react-router-dom";
+import Login from './components/login';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <Routes>
-      
+     <Route path="/" element={<Login />} />
      </Routes>
     </>
   )
