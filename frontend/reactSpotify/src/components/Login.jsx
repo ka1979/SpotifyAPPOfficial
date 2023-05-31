92d79122b39c987d5651747a2e625a46f50ae8c0
ka1979 <118313969+ka1979@users.noreply.github.com>
2023-05-31 14:45:15 -0400

    added spotify to login

--- frontend/reactSpotify/src/components/Login.jsx
+++ frontend/reactSpotify/src/components/Login.jsx
@@ -3,9 +3,10 @@ const Login=()=>{
 
 
     return  (
-       
-        <img src={"https://images.squarespace-cdn.com/content/v1/59a6fd41bebafb8b3f420544/1519917998014-G6SW42V49ZA7JSDOZJN3/image-asset.png?format=1000w"}/>
-       
+        <>
+       <a href="localhost:3000/login" >
+        <img src={"https://images.squarespace-cdn.com/content/v1/59a6fd41bebafb8b3f420544/1519917998014-G6SW42V49ZA7JSDOZJN3/image-asset.png?format=1000w"}/></a>
+        </>
     )
 }
 export default Login
