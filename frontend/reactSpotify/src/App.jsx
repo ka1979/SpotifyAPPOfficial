b6b850a014b8ee680ccffae518cdb0bafc7dd5c5
simonanderson16 <uhx8bu@virginia.edu>
2023-05-31 16:09:00 -0400

    get current user

--- frontend/reactSpotify/src/App.jsx
+++ frontend/reactSpotify/src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from './components/login';
-import PageNotFound from './components/PageNotFound/PageNotFound';
+import Login from "./components/login";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import HomeSY from "./components/HomeSY";
 function App() {
-
-
-  const [loggedIn, setLoggedIN]=useState(false)
+  const [loggedIn, setLoggedIN] = useState(false);
   return (
     <>
-     <Routes>
-     <Route path="/" element={<Login />} />
-  
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homeSY" element={<HomeSY />} />
 
-     <Route path="*" element={<PageNotFound />} />
-     </Routes>
-
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
