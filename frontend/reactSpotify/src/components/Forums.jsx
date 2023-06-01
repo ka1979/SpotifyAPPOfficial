299336cf40e83955fb14a65ddece5d0e36e93bd2
simonanderson16 <uhx8bu@virginia.edu>
2023-06-01 17:39:07 -0400

    fixed user and nav bar in forums page

--- frontend/reactSpotify/src/components/Forums.jsx
+++ frontend/reactSpotify/src/components/Forums.jsx
@@ -1,21 +1,35 @@
-
-import React, { useEffect, useState } from 'react';
-import { Container, Grid, Card, CardContent, Typography, TextField, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Box } from '@mui/material';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Box,
+} from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import axios from "axios";
 import NavigationBar from "./Navbar";
-import {user} from "./LandingPage";
+import { user } from "./LandingPage";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppStateContext } from "../AppState";
 
-import Helmet from "react-helmet"
-
-
-
-
-
+import Helmet from "react-helmet";
 
 const Forums = () => {
-  const email = user;
+  const { appState, setAppState } = useContext(AppStateContext);
+  let email = appState.user;
+  if (!appState.user) {
+    email = localStorage.getItem("email");
+  }
   const [forums, setForums] = useState([]);
   const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
@@ -27,10 +41,10 @@ const Forums = () => {
 
   const fetchForums = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/forums/get'); 
+      const response = await axios.get("http://localhost:3000/forums/get");
       setForums(response.data);
     } catch (error) {
-      console.error('Error fetching forums:', error);
+      console.error("Error fetching forums:", error);
     }
   };
 
@@ -52,19 +66,24 @@ const Forums = () => {
 
   const handleCreateForum = async () => {
     try {
-      const creatorRes = await axios.post('http://localhost:3000/forums/creator', {email: email});
+      const creatorRes = await axios.post(
+        "http://localhost:3000/forums/creator",
+        { email: email }
+      );
       const creator = creatorRes.data.result;
-      const response = await axios.post('http://localhost:3000/forums/post', { title: newForumTitle, creator: creator });
+      const response = await axios.post("http://localhost:3000/forums/post", {
+        title: newForumTitle,
+        creator: creator,
+      });
       console.log(response.data);
-      
+
       fetchForums();
       setNewForumTitle("");
       setOpen(false);
     } catch (error) {
-      console.error('Error creating forum:', error);
+      console.error("Error creating forum:", error);
     }
   };
-  
 
   const filteredForums = forums.filter((forum) =>
     forum.title.toLowerCase().includes(search.toLowerCase())
@@ -76,82 +95,86 @@ const Forums = () => {
   //   const differenceInDays = differenceInTime / (1000 * 3600 * 24);
   //   return Math.floor(differenceInDays);
   // }
-  
 
-  
   return (
     <>
-
-    <NavigationBar />
-    <div className="view-container">
-    <Container>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        label="Search forums"
-        value={search}
-        onChange={handleSearchChange}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<AddCircleOutlineIcon sx={{color:'white'}}/>}
-        style={{ margin: '10px 0' }}
-        onClick={handleOpen}
-      >
-        Create New Forum
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create a new forum</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To create a new forum, please enter the forum title here.
-          </DialogContentText>
+      <NavigationBar page="forums" />
+      <div className="view-container">
+        <Container>
           <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Forum Title"
-            type="text"
+            variant="outlined"
+            margin="normal"
             fullWidth
-            value={newForumTitle}
-            onChange={handleForumTitleChange}
+            label="Search forums"
+            value={search}
+            onChange={handleSearchChange}
           />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddCircleOutlineIcon sx={{ color: "white" }} />}
+            style={{ margin: "10px 0" }}
+            onClick={handleOpen}
+          >
+            Create New Forum
           </Button>
-          <Button onClick={handleCreateForum} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/* <Grid container spacing={3}> */}
-        {filteredForums.map((forum) => (
-          // <Grid item key={forum.id} xs={12} sm={6} md={4}>
-          <Box mb={2} borderBottom={1} borderColor="divider" key={forum.id}>
-            <Link to={`/posts/${forum.id}`} style={{ textDecoration: 'none', color: 'white' }}>
-            <Card >
-              <CardContent>
-                <Typography variant="h5">{forum.title}</Typography>
-                <Typography variant="body2">Created by: {forum.creator}</Typography>
-              </CardContent>
-            </Card>
-            </Link>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Create a new forum</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To create a new forum, please enter the forum title here.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Forum Title"
+                type="text"
+                fullWidth
+                value={newForumTitle}
+                onChange={handleForumTitleChange}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleCreateForum} color="primary">
+                Create
+              </Button>
+            </DialogActions>
+          </Dialog>
+          {/* <Grid container spacing={3}> */}
+          {filteredForums.map((forum) => (
+            // <Grid item key={forum.id} xs={12} sm={6} md={4}>
+            <Box mb={2} borderBottom={1} borderColor="divider" key={forum.id}>
+              <Link
+                to={`/posts/${forum.id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5">{forum.title}</Typography>
+                    <Typography variant="body2">
+                      Created by: {forum.creator}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Link>
             </Box>
-          // </Grid>
-        ))}
-      {/* </Grid> */}
-    </Container>
+            // </Grid>
+          ))}
+          {/* </Grid> */}
+        </Container>
 
-    <Helmet>
-      <title>Forums</title>
-    </Helmet>
-    
-
-    </div>
+        <Helmet>
+          <title>Forums</title>
+        </Helmet>
+      </div>
     </>
   );
 };
--- frontend/reactSpotify/src/components/Posts.jsx
+++ frontend/reactSpotify/src/components/Posts.jsx
@@ -1,7 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import {
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
 import NavigationBar from "./Navbar";
 
 const Posts = () => {
@@ -19,7 +27,7 @@ const Posts = () => {
       const response = await axios.get(`http://localhost:3000/posts/${id}`);
       setPosts(response.data);
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      console.error("Error fetching posts:", error);
     }
   };
 
@@ -33,65 +41,76 @@ const Posts = () => {
 
   const handleCreatePost = async () => {
     try {
-      await axios.post('http://localhost:3000/posts', { forumId: id, title: newPostTitle, creator: 'Your name here' });
+      await axios.post("http://localhost:3000/posts", {
+        forumId: id,
+        title: newPostTitle,
+        creator: "Your name here",
+      });
       setNewPostTitle("");
       handleClose();
       fetchPosts();
     } catch (error) {
-      console.error('Error creating post:', error);
+      console.error("Error creating post:", error);
     }
   };
 
   const handleLike = async (postId) => {
     try {
-      await axios.post(`http://localhost:3000/posts/${id}/like`, { forumId: id, postId });
+      await axios.post(`http://localhost:3000/posts/${id}/like`, {
+        forumId: id,
+        postId,
+      });
       fetchPosts();
     } catch (error) {
-      console.error('Error liking post:', error);
+      console.error("Error liking post:", error);
     }
-  }
+  };
 
   return (
     <>
-    <NavigationBar />
-    <div className="view-container">
-      <Button variant="contained" color="primary" onClick={handleOpen}>
-        Add New Post
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create a new post</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To create a new post, please enter the post title here.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Post Title"
-            type="text"
-            fullWidth
-            value={newPostTitle}
-            onChange={(e) => setNewPostTitle(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleCreatePost} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>Likes: {post.likes}</p>
-          <button onClick={() => handleLike(post.id)}>Like</button>
-        </div>
-      ))}
-    </div>
+      <NavigationBar page="forums" />
+      <div className="view-container">
+        <Button variant="contained" color="primary" onClick={handleOpen}>
+          Add New Post
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Create a new post</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To create a new post, please enter the post title here.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Post Title"
+              type="text"
+              fullWidth
+              value={newPostTitle}
+              onChange={(e) => setNewPostTitle(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleCreatePost} color="primary">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>Likes: {post.likes}</p>
+            <button onClick={() => handleLike(post.id)}>Like</button>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
