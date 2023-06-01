6592852ee04cbcfcb65e7bdbca5f330396a7732e
simonanderson16 <uhx8bu@virginia.edu>
2023-05-31 22:16:42 -0400

    login and landing pages

--- frontend/reactSpotify/src/components/Login.jsx
+++ frontend/reactSpotify/src/components/Login.jsx
@@ -4,16 +4,20 @@ import { styled, Button } from "@mui/material";
 const Login = () => {
   const LoginButton = styled(Button)(({ theme }) => ({
     color: "black",
+    backgroundColor: "white",
     fontWeight: "bold",
     "&:hover": {
-      color: theme.palette.secondary.main,
+      //   color: theme.palette.secondary.main,
+      color: "white",
       boxShadow: "0 0 10px #537FE7, 0 0 20px #537FE7, 0 0 30px #537FE7",
+      backgroundColor: "#1DB954",
+      outline: "solid 1px black",
     },
     "&:focus": {
-      outline: "none",
+      outline: "black",
     },
-    height: "30px",
-    margin: "0 10px",
+    // height: "30px",
+    margin: "30px 10px 20px 10px",
   }));
 
   const handleLogin = () => {
@@ -21,12 +25,26 @@ const Login = () => {
   };
 
   return (
-    <>
-      <LoginButton onClick={handleLogin}>
-        <LoginIcon />
+    <div className="blue-container">
+      <h1 className="welcome-text">Welcome to Spotify Social!</h1>
+      <div className="shadow-container">
+        <h3>
+          To use our app, you must sign in with your Spotify account by clicking
+          the button below
+        </h3>
+      </div>
+      <LoginButton
+        // style={{
+        //   margin: "30px 10px 10px 10px",
+        //   color: "black",
+        //   backgroundColor: "white",
+        // }}
+        onClick={handleLogin}
+      >
+        <LoginIcon style={{ marginRight: "10px" }} />
         Login with Spotify
       </LoginButton>
-    </>
+    </div>
   );
 };
 
