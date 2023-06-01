2848b1146792d7f491ed21bf05a34349b4229ca0
yash-d99 <dubey.yash2@gmail.com>
2023-06-01 11:37:49 -0400

    working profile page

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var spotifyRouter = require("./routes/spotify");
 var databaseRouter = require("./routes/database");
+var yashRouter = require("./routes/yash");
 
 var app = express();
 
@@ -41,6 +42,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/spotify", spotifyRouter);
 app.use("/database", databaseRouter);
+app.use("/yash", yashRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
