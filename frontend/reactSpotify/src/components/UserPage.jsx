a0fab6d82e2e9cfd305e5d87cc73855cec145609
simonanderson16 <uhx8bu@virginia.edu>
2023-06-01 11:07:55 -0400

    user page done

--- frontend/reactSpotify/src/components/UserPage.jsx
+++ frontend/reactSpotify/src/components/UserPage.jsx
@@ -3,6 +3,7 @@ import NavigationBar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Button } from "@mui/material";
 
 export default function UserPage() {
   let { email } = useParams();
@@ -11,6 +12,10 @@ export default function UserPage() {
   const [topSongs, setTopSongs] = useState([]);
   const [topArtists, setTopArtists] = useState([]);
   const [likedSongs, setLikedSongs] = useState([]);
+
+  const [showingTopSongs, setShowingTopSongs] = useState(false);
+  const [showingTopArtists, setShowingTopArtists] = useState(false);
+  const [showingLikedSongs, setShowingLikedSongs] = useState(true);
 
   const getUserInfo = async () => {
     try {
@@ -34,10 +39,84 @@ export default function UserPage() {
     <>
       <NavigationBar />
       <div className="view-container">
-        <h1>{name}</h1>
-        <p>{email}</p>
-        <p>{name}</p>
-        {console.log(topSongs)}
+        <h1 className="white-text">{name}</h1>
+        <div className="time-buttons">
+          <Button
+            style={{ margin: "10px" }}
+            variant="contained"
+            onClick={() => {
+              setShowingLikedSongs(true);
+              setShowingTopArtists(false);
+              setShowingTopSongs(false);
+            }}
+          >
+            Liked Songs
+          </Button>
+          <Button
+            style={{ margin: "10px" }}
+            variant="contained"
+            onClick={() => {
+              setShowingLikedSongs(false);
+              setShowingTopArtists(false);
+              setShowingTopSongs(true);
+            }}
+          >
+            Top Songs
+          </Button>
+          <Button
+            style={{ margin: "10px" }}
+            variant="contained"
+            onClick={() => {
+              setShowingLikedSongs(false);
+              setShowingTopArtists(true);
+              setShowingTopSongs(false);
+            }}
+          >
+            Top Artists
+          </Button>
+        </div>
+        {showingLikedSongs && (
+          <div className="cards-container">
+            {likedSongs.map((track, index) => (
+              <div className="top-card">
+                <img src={track.image}></img>
+                <div className="description">
+                  <p className="top-title">{track.title}</p>
+                  {track.artists.map((artist, index) => (
+                    <p>{artist}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {showingTopSongs && (
+          <div className="cards-container">
+            {topSongs.map((track, index) => (
+              <div className="top-card">
+                <img src={track.image}></img>
+                <div className="description">
+                  <p className="top-title">{track.title}</p>
+                  {track.artists.map((artist, index) => (
+                    <p>{artist}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {showingTopArtists && (
+          <div className="cards-container">
+            {topArtists.map((artist, index) => (
+              <div className="top-card">
+                <img src={artist.image}></img>
+                <div className="description">
+                  <p className="top-title">{artist.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
