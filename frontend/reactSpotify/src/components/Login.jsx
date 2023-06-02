1915db2e978f1d05ec4fcd24a3a718f5437fcfac
simonanderson16 <uhx8bu@virginia.edu>
2023-06-02 09:36:14 -0400

    login page image

--- frontend/reactSpotify/src/components/Login.jsx
+++ frontend/reactSpotify/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import LoginIcon from "@mui/icons-material/Login";
 import { styled, Button } from "@mui/material";
 import { Helmet } from "react-helmet";
+import logo from "../assets/logo.png";
+
 const Login = () => {
   const LoginButton = styled(Button)(({ theme }) => ({
     color: "black",
@@ -24,30 +26,43 @@ const Login = () => {
     window.location.href = "http://localhost:3000/spotify/login";
   };
 
-  return (<>
-    <Helmet>
-    <title>Login</title>
-  </Helmet>
-    <div className="blue-container">
-      <h1 className="welcome-text">Welcome to Spotify Social!</h1>
-      <div className="shadow-container">
-        <h3>
-          To use our app, you must sign in with your Spotify account by clicking
-          the button below
-        </h3>
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      {/* <div className="view-container"></div> */}
+      <div className="view-container">
+        <div className="cards-container">
+          <div className="top-card" style={{ backgroundColor: "transparent" }}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1540/1540794.png"
+              style={{ marginBottom: "0" }}
+            ></img>
+          </div>
+        </div>
       </div>
-      <LoginButton
-        // style={{
-        //   margin: "30px 10px 10px 10px",
-        //   color: "black",
-        //   backgroundColor: "white",
-        // }}
-        onClick={handleLogin}
-      >
-        <LoginIcon style={{ marginRight: "10px" }} />
-        Login with Spotify
-      </LoginButton>
-    </div>
+
+      <div className="blue-container">
+        <h1 className="welcome-text">Welcome to Spotify Social!</h1>
+        <div className="shadow-container">
+          <h3>
+            To use our app, you must sign in with your Spotify account by
+            clicking the button below
+          </h3>
+        </div>
+        <LoginButton
+          // style={{
+          //   margin: "30px 10px 10px 10px",
+          //   color: "black",
+          //   backgroundColor: "white",
+          // }}
+          onClick={handleLogin}
+        >
+          <LoginIcon style={{ marginRight: "10px" }} />
+          Login with Spotify
+        </LoginButton>
+      </div>
     </>
   );
 };
